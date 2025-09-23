440aaf7e6d40ea7205645574a0ef5a1d44a024fd
JustinBenito <justinbenito1974@gmail.com>
2025-09-23 20:15:39 +0530

    aama orey luv dhaan

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className='flex w-full flex-1 items-center justify-center bg-[#fafafa] px-4 py-4 text-black md:px-8 lg:px-16'>
         <p className='mb-2 mt-16 text-center text-gray-600'>
-          Made with joy by from Justin 💚 from
+          Made with joy by Justin with luv 💚 from
           <a href='https://fossunited.org/c/chennai' className='ml-2 text-[#03b051]'>
             FOSS United Chennai
           </a>
